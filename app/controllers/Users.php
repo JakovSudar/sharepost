1b3dfa6665f33bc463fb8a244240b45465642621
JakovSudar <jakovsudar1@gmail.com>
2019-09-23 21:34:05 +0200

    Form validation added

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -6,10 +6,48 @@
         public function register(){
             //check for POST
             if($_SERVER['REQUEST_METHOD']=='POST'){
-                //Process form        
+                //Process form  
+
+                //sanitize post data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data=[
+                    'name'=>trim($_POST['name']),
+                    'email' =>trim($_POST['email']),
+                    'password' =>trim($_POST['password']),
+                    'confirm_password' =>trim($_POST['confirm_password']),
+                    'name_error' => '',
+                    'email_error' => '',
+                    'password_error' => '',
+                    'confirm_password_error' =>''
+                ];    
+                //validation
+                if(empty($data['email'])){
+                    $data['email_error']='Please enter email';
+                }  
+                if(empty($data['name'])){
+                    $data['name_error']='Please enter name';
+                } 
+                if(empty($data['password'])){
+                    $data['password_error']='Please enter password';
+                } else if(strlen($data['password'])<6){
+                    $data['password_error']='Password must be at least 6 characters';
+                }
+                if(empty($data['confirm_password'])){
+                    $data['confirm_password_error']='Please confirm password';
+                }else{
+                    if($data['password']!=$data['confirm_password']){
+                        $data['confirm_password_error']='Passwords do not match';
+                    }
+                }  
+                if(empty($data['email_error'])&&empty($data['name_error'])&&empty($data['password_error'])&&empty($data['confirm_password_error'])){
+                    die('SUCCESS');
+                }else{
+                    $this->view('users/register',$data);
+                }
+                
             }else
-            {
-                //init data
+            {                
                 $data=[
                     'name'=>'',
                     'email' =>'',
@@ -27,12 +65,33 @@
         public function login(){
             //check for POST
             if($_SERVER['REQUEST_METHOD']=='POST'){
-                //Process form        
+                //Process form   
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data=[                    
+                    'email' =>trim($_POST['email']),
+                    'password' =>trim($_POST['password']),                                     
+                    'email_error' => '',
+                    'password_error' => ''                    
+                ];    
+
+
+                //validation     
+                if(empty($data['email'])){
+                    $data['email_error']='Please enter email';
+                }  
+                if(empty($data['password'])){
+                    $data['password_error']='Please enter password';
+                }
+                if(empty($data['email_error'])&&empty($data['password_error'])){
+                    die('SUCCESS');
+                }else{
+                    $this->view('users/login',$data);
+                }
             }else
             {
                 //init data
-                $data=[
-                    
+                $data=[                    
                     'email' =>'',
                     'password' =>'',                     
                     'email_error' => '',
