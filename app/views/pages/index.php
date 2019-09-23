4dba7ed208f831c16500863a6aea2a3d1cfeb638
JakovSudar <jakovsudar1@gmail.com>
2019-09-23 17:59:43 +0200

    Login/register forms added

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,14 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <h1><?php echo $data['title']; ?></h1>
   
+  <div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+      <h1 class="display-3">Moji psi</h1>
+      <p class="lead">New way of life</p>
+      <hr class="my-2">
+      <p>What are you waiting?</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg " href="Jumbo action link" role="button">Start posting!</a>
+      </p>
+    </div>
+  </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
