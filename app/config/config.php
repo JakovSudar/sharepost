44f95e20c5f7020b29cbe7c4399c0c21cf7c4ce2
JakovSudar <jakovsudar1@gmail.com>
2019-09-23 15:08:08 +0200

    config adjusted
    inserted database name and rerouting

--- app/config/config.php
+++ app/config/config.php
@@ -3,11 +3,11 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', 'jakov123j');
-  define('DB_NAME', 'mvc');
+  define('DB_NAME', 'sharepost');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/traversymvc');
+  define('URLROOT', 'http://localhost/sharepost');
   // Site Name
-  define('SITENAME', 'TraversyMVC');
+  define('SITENAME', 'SharePost');
